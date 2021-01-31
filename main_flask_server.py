731a1e93979836c910bfd6ba0fe5d9000d9f57f7
abel <abelarregui@gmail.com>
2021-01-31 13:46:24 +0100

    flask has been installed in pipenv environment. Docstrings in flask main.

--- main_flask_server.py
+++ main_flask_server.py
@@ -4,11 +4,15 @@ from data_persistence.mongodb import MongoDB
 app = Flask(__name__)
 
 
-# Source:
+# Reference:
 # https://medium.com/@ishmeet1995/how-to-create-restful-crud-api-with-python-flask-mongodb-and-docker-8f6ccb73c5bc
 
 @app.route('/')
 def base():
+    """
+    Base webpage: status UP
+    :return:
+    """
     return Response(response=json.dumps({"Status": "UP"}),
                     status=200,
                     mimetype='application/json')
@@ -16,6 +20,15 @@ def base():
 
 @app.route('/musicalworks', methods=['GET'])
 def mongo_read():
+    """
+    GET endpoint. A dictionary is needed for input parameter. Example:
+        {
+        "db": "bmat",
+        "collection": "musicalworks",
+        "iswc": ["T0426508306", "T0420889173"]
+        }
+    :return: json with right owners info for the given iswc's
+    """
     data = request.json
     if data is None or data == {}:
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
@@ -29,4 +42,4 @@ def mongo_read():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=True, port=5001, host='127.0.0.1')
